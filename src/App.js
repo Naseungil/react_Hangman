aa284900e84beb6ce6ba3e9bbd57f6c41d53cbd4
Naseungil <sktmddlf505@naver.com>
2024-11-01 17:47:36 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import GameBoard from './GameBoard';
+import SetWord from './SetWord';
+import {
+  Routes,
+  Route
+} from "react-router-dom";
+
+function App() {
+  const [maxError, setMaxError] = useState(0);
+  const [answerLength, setAnswerLength] = useState(0);
+  const [secretWord, setSecretWord] = useState('');
+
+  useEffect(()=>{
+    let word = window.localStorage.getItem('scretWord');    
+    if(word && word.length > 0 ){
+      setSecretWord(word);
+      setAnswerLength(word.length);
+      setMaxError(word.length + 2)
+    }else{
+      setAnswerLength(0);
+      setMaxError(1)
+    }
+  },[]);
+
+  return (
+    <div className="App">
+      <h1>Hangman</h1>
+      <p>Do you want to play game</p>
+
+      <Routes>
+        <Route path="/" element={<GameBoard secretWord={secretWord} maxError={maxError} answerLength={answerLength}/>} />
+        <Route path="/admin" element={<SetWord />} />
+      </Routes> 
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/LetterGrid.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import Letter from './Letter';
+
+const LetterGrid = ({secretWord, guessedLetters, answerLength, complete})=>{
+
+  //let letters = secretWord.split('').map(letter=><span>{letter}</span>);
+
+  //secretWord의 문자열 배열로 변환하고, 그 배열에 각각의 값으로 <span></span>
+  const [answer, setAnswer] = useState(0);
+  const [isComplete, setIsComplete] = useState(false);
+
+
+  //answer값이 변경되면 answerLength와 비교해서 정답여부 파악
+  useEffect(()=>{
+    if(answerLength > 0 && answer === answerLength && !isComplete){
+      alert('정답입니다!');
+      complete();
+      setIsComplete(true);
+    }
+  },[answer, answerLength, complete, isComplete]);
+
+  //guessedLetters값이 변경되면 answer를 업데이트
+  useEffect(()=>{
+    
+    let newCount = [...secretWord].reduce((count,letter)=>{
+      return count + (guessedLetters.includes(letter.toLowerCase())? 1:0);
+    },0);
+    setAnswer(newCount);
+
+    console.log('실행', newCount);
+
+  },[guessedLetters, secretWord])
+
+  let letters = [...secretWord].map((letter, idx)=>{
+    //let isShown = guessedLetters.indexOf(letter.toLowerCase()) > -1;
+    let isShown = guessedLetters.includes(letter.toLowerCase());
+    return(
+      <Letter key={idx} value={letter} isShown={isShown}></Letter>
+    )
+});
+  
+  return (
+    <div className="letters">
+
+      {letters}
+     
+    </div>
+  );
+}
+
+export default LetterGrid;
